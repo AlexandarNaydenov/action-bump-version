3a9547ebc0e11e94386c6622078ade2115a880d4
Alexandar Naydenov <alexandar.naydenov@sap.com>
2021-05-19 15:48:10 +0300

    testing semver

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const semver = require('semver');
+
 
 try {
     let version = core.getInput('input-version');
     console.log(version);
-    version = '1.0.1';
+    version = semver.inc(version, major);
     core.setOutput('output-version', version);
+
 } catch (error) {
     core.setFailed(error.message);
 }
