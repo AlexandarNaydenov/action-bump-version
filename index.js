4ccf1641baec3dfbffc4bd1b94fdb8d5e054b61b
Alexandar Naydenov <alexandar.naydenov@sap.com>
2021-05-19 15:37:46 +0300

    fix output message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
     let version = core.getInput('input-version');
-    console.log('${version}');
+    console.log(version);
     version = '1.0.1';
     core.setOutput('output-version', version);
 } catch (error) {
