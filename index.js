e0c5e6ae7769b8d8c1cbc704d84d1ae7e76c2372
Alexandar Naydenov <alexandar.naydenov@sap.com>
2021-05-20 10:28:06 +0300

    Separate js on 3 parts

--- index.js
+++ index.js
@@ -2,29 +2,38 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const semver = require('semver');
 const fs = require('fs');
+let version;
 
+//reading version from VERSION file
 try {
-    //reading version from VERSION file
     const data = fs.readFileSync('./VERSION', 'utf8');
-    let version = data;
-    //Incremention version
-    version = semver.inc(version, 'minor');
-    core.setOutput('output-version', version);
-    fs.writeFileSync('./VERSION', version);
-
+    version = data;
 } catch (error) {
     core.setFailed(error.message);
 }
 
+//Incremention version
 try {
     const event = github.context.payload
 
     if (!event.commits) {
         console.log('Couldn\'t find any commits in this event, incrementing patch version...')
     }
-    const messages = event.commits ? event.commits.map(commit => commit.message) : [];
-    console.log(messages);
+    const message = event.commits ? event.commits.map(commit => commit.message) : [];
+    console.log(message);
+
+    version = semver.inc(version, 'minor');
 }
 catch (error) {
+    core.setFailed(error.message);
+}
+
+//Output and change VERSION
+try{
+
+    core.setOutput('output-version', version);
+    fs.writeFileSync('./VERSION', version);
+
+}catch (error) {
     core.setFailed(error.message);
 }
